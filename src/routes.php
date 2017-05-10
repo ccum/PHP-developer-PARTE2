31afc75803e9652d50e279d2755ff4b946458616
Cesar Cueva <cueva1487@gmail.com>
2017-05-10 04:50:33 -0500

    Correcto

--- src/routes.php
+++ src/routes.php
@@ -1,6 +1,7 @@
 <?php
 // Routes
 
+
 $app->get('/', function ($request, $response, $args) {
     // Sample log message
     $this->logger->info("Slim-Skeleton '/' route");
@@ -15,7 +16,8 @@ $app->get('/employees', function ($request, $response, $args) {
     $str = file_get_contents(__DIR__ . '/employees.json');
 	$listEmployees = (array)json_decode($str,TRUE);
 	return $this->renderer->render($response, 'employees.phtml', ["listEmployees" => $listEmployees]);
-});
+})->setName('employees');
+
 $app->get('/employeDetail/{id}', function ($request, $response, $args){
     $str = file_get_contents(__DIR__ . '/employees.json');
 	$listEmployees = (array)json_decode($str,TRUE);
@@ -32,4 +34,26 @@ $app->get('/employeDetail/{id}', function ($request, $response, $args){
 	}
 	$this->logger->info("Buscar '/' ". $employeeReturn['name']);
 	return $this->renderer->render($response, 'employeDetail.phtml', ["employeeReturn" => $employeeReturn]);
+})->setName('employeeDetail');
+
+$app->get('/webservice/searchByRangeSalary/{min}/{max}', function ($request, $response, $args){
+    
+	$min = floatval($args['min']);
+	$max = floatval($args['max']);
+    $str = file_get_contents(__DIR__ . '/employees.json');
+	$listEmployees = (array)json_decode($str,TRUE);
+	$listResult = array();
+	foreach ($listEmployees as $employee)
+	{
+   		$tempSalary = str_replace("$", "", $employee["salary"]);
+   		$tempSalary = floatval(str_replace(",", "", $tempSalary));
+
+   		$this->logger->info("salario float '/' ". $tempSalary);
+    	if($tempSalary > $min and $tempSalary < $max)
+    	{
+    		array_push($listResult, (array)$employee);
+
+		}
+	}
+	return $this->renderer->render($response, 'searchByRangeSalary.xml', ["listResult" => $listResult]);
 });
